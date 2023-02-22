d1867bcdfd6a5658d7df41329a1c56a5f92e1693
Ana2828Bog <ana.bog2888@gmail.com>
2023-02-21 19:53:42 -0500

    this is changes

---
+++ src/practice_for_git/GitPractice2.java
@@ -0,0 +1,12 @@
+package practice_for_git;
+
+public class GitPractice2 {
+
+	public static void main(String[] args) {
+		String a = "This is for Push practice";
+		
+		System.out.println(a);
+
+	}
+
+}
